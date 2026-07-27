eb71e12de13b11339684aea4846501e71f8a2ade
Yehmeg <gomtikumari26@gmail.com>
2026-07-27 22:54:04 +0530

    Time: 0 ms (100%), Space: 28.2 MB (70.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority=nums[1];
-
-        for(int i=0; i< nums.size(); i++){
-            int j=i;
-            while (j>=1 && nums[j]< nums[j-1]){
-                swap (nums[j],nums[j-1]);
-                j--;
-            }
-
-        }
-
-        return nums[nums.size() / 2];
-        
+       sort(nums.begin(),nums.end()); 
+       return nums[(nums.size())/2];
     }
 };
