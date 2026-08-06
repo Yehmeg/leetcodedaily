5f632964a3ba832b06cba19f85d20da01beec094
Yehmeg <gomtikumari26@gmail.com>
2026-08-06 22:07:46 +0530

    Time: 0 ms (100%), Space: 71.7 MB (67.96%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n=s.size();
-        long long sum = 0;
+        long sum = 0;
 
         for(int i = n-1; i >= 0; i--){
-            sum = (sum + shifts[i]) % 26;
+            sum +=shifts[i] ;
 
             s[i] = 'a' + (s[i] - 'a' + sum) % 26;
         }
