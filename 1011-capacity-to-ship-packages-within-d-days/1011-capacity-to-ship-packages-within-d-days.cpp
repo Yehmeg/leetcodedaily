921f0924f43e19517e765688e6e87bc0ea2816bd
Yehmeg <gomtikumari26@gmail.com>
2026-08-08 23:01:11 +0530

    Time: 15 ms (29.65%), Space: 35 MB (89.17%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    bool check_cap(int mid,vector<int>weights,int days){
-        int n=weights.size();
-        int m=mid;
-        int count=1;
-        for(int i=0;i<n;i++){
-            if(m>=weights[i]) m-=weights[i];
-            else {
-                m=mid;
-                count++;
-                m-=weights[i];
-            }
-  
-        }
-       
-        if(count<=days) return true;
-        else return false;
-    }
+
     int shipWithinDays(vector<int>& weights, int days) {
         int lo=INT_MIN;
         int hi= 0;
@@ -26,11 +10,24 @@ public:
             lo=max(weights[i],lo);
         }
         while(lo<=hi){
+
             int mid=lo+(hi-lo)/2;
-            if(check_cap(mid,weights,days)){
+            int n=weights.size();
+            int m=mid;
+            int count=1;
+            for(int i=0;i<n;i++){
+                if(m>=weights[i]) m-=weights[i];
+                else {
+                    m=mid;
+                    count++;
+                    m-=weights[i];
+                }   
+            }     
+            if(count<=days)  {
                 ans=mid;
                 hi=mid-1;
             }
+         
             else{
                 lo=mid+1;
             }
