c50821e523725099c6442165fa233568818183a8
Yehmeg <gomtikumari26@gmail.com>
2026-08-11 20:20:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (15.54%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        int r = sqrt(area);
+        int r= sqrt(area);
 
-        vector<int> ans(2);
-
-        while(r >= 1) {
-
-            if(area % r == 0) {
-                ans[0] = area / r;   // l
-                ans[1] = r;
-                return ans;
-            }
-
+        while (area % r != 0)
             r--;
-        }
 
-        return ans;
+        return {area / r, r};
     }
 };
