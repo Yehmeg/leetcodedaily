36376df6e761a9f3e312045eaa57aa07edd547e8
Yehmeg <gomtikumari26@gmail.com>
2026-08-15 18:39:20 +0530

    Time: 47 ms (32.57%), Space: 85.9 MB (75.92%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -3,12 +3,12 @@ public:
     int maximumGap(vector<int>& nums) {
         int n=nums.size();
         if(n<2) return 0;
-        
+
         sort(nums.begin(),nums.end());
         int ans=INT_MIN;
         for(int i=0;i<n-1;i++){
-            int temp=nums[i+1]-nums[i];
-            ans=max(ans,temp);
+            
+            ans=max(ans,nums[i+1]-nums[i]);
         }
         return ans;
     }
