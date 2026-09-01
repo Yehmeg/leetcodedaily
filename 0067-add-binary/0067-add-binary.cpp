dd88f625c129f78fde16a13d854dbc79833594a8
Gomti Kumari <gomtikumari26@gmail.com>
2026-09-02 00:07:38 +0530

    Time: 0 ms (100%), Space: 9.2 MB (24.28%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -5,19 +5,25 @@ public:
         string ans;
         char carry = '0';
 
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        if(a.size() < b.size()) {
+            a = string(b.size() - a.size(), '0') + a;
+        }
+        else if(b.size() < a.size()) {
+            b = string(a.size() - b.size(), '0') + b;
+        }
 
         // Common part
-        while(i >= 0 && j >= 0) {
+        int i = a.size()-1;
+
+        while(i >= 0 ) {
 
             if(carry == '0') {
 
-                if(a[i] == '1' && b[j] == '1') {
+                if(a[i] == '1' && b[i] == '1') {
                     carry = '1';
                     ans.push_back('0');
                 }
-                else if(a[i] == '1' || b[j] == '1') {
+                else if(a[i] == '1' || b[i] == '1') {
                     carry = '0';
                     ans.push_back('1');
                 }
@@ -29,11 +35,11 @@ public:
 
             else {
 
-                if(a[i] == '1' && b[j] == '1') {
+                if(a[i] == '1' && b[i] == '1') {
                     carry = '1';
                     ans.push_back('1');
                 }
-                else if(a[i] == '1' || b[j] == '1') {
+                else if(a[i] == '1' || b[i] == '1') {
                     carry = '1';
                     ans.push_back('0');
                 }
@@ -44,50 +50,8 @@ public:
             }
 
             i--;
-            j--;
-        }
-
-        // Remaining part of a
-        while(i >= 0) {
-
-            if(carry == '1') {
-
-                if(a[i] == '1') {
-                    ans.push_back('0');
-                    carry = '1';
-                }
-                else {
-                    ans.push_back('1');
-                    carry = '0';
-                }
-            }
-            else {
-                ans.push_back(a[i]);
-            }
-
-            i--;
-        }
-
-        // Remaining part of b
-        while(j >= 0) {
-
-            if(carry == '1') {
-
-                if(b[j] == '1') {
-                    ans.push_back('0');
-                    carry = '1';
-                }
-                else {
-                    ans.push_back('1');
-                    carry = '0';
-                }
-            }
-            else {
-                ans.push_back(b[j]);
-            }
-
-            j--;
-        }
+            
+        }  
 
         if(carry == '1')
             ans.push_back('1');
