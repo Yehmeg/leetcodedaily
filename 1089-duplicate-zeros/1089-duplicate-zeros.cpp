d54b13c43504d197a95b29f2d3e542978b686399
Gomti Kumari <gomtikumari26@gmail.com>
2026-09-11 10:56:59 +0530

    Time: 0 ms (100%), Space: 14.1 MB (34.07%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -3,14 +3,10 @@ public:
     void duplicateZeros(vector<int>& arr) {
         int n= arr.size();
        for( int i =0 ; i<n; i++){
-            if( arr[i]== 0){
-                
-                arr.push_back(0);
-                i++;
-                reverse(arr.begin()+i, arr.end());
-                // i++;
-                reverse(arr.begin()+i+1, arr.end());
-                arr.pop_back();               
+            if( arr[i]== 0){   
+                arr.insert(arr.begin()+i, 0);
+                arr.pop_back();  
+                i++;             
             }
 
        } 
