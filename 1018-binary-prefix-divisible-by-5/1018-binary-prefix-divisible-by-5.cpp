f031b17c7c75a183bdee784653b5fa1468806b17
Gomti Kumari <gomtikumari26@gmail.com>
2026-09-14 20:10:45 +0530

    Time: 0 ms (100%), Space: 18.5 MB (10.83%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -4,16 +4,12 @@ public:
     vector<bool> prefixesDivBy5(vector<int>& nums) {
         vector<bool> ans(nums.size(), false);
         string binarys;
+        int rem=0;
 
         for( int i =0; i<nums.size(); i++)
-        {   int rem = 0;
-
-            char ch=nums[i] + '0' ;
-            binarys.push_back(ch);
-
-            for (char bit : binarys) {
-                rem= (rem * 2 + (bit - '0')) % 5;
-            }
+        {   
+            char bit=nums[i] + '0' ;
+            rem= (rem * 2 + (bit - '0')) % 5;        
             if(rem==0) 
                 ans[i]=true;
         }
