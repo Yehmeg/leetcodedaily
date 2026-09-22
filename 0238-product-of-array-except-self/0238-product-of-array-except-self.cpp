39580f449a55827cf53279c5cd27807d88dacb53
Gomti Kumari <gomtikumari26@gmail.com>
2026-09-23 00:51:13 +0530

    Time: 0 ms (100%), Space: 40.1 MB (95.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-
-        int n = nums.size();
-        vector<int> ans(n, 1);
-
-        // Product of everything to the left
-        int left = 1;
-        for(int i = 0; i < n; i++) {
-            ans[i] = left;
-            left *= nums[i];
+        
+        int prod=1;
+        int pz= 1;
+        int count =0;
+        for( int i=0; i< nums.size(); i++){        
+            if(nums[i]==0) count ++;
+            prod *= nums[i];
+            if(nums[i]!=0) pz *= nums[i];
+        }       
+        if(count>1) pz=0; 
+        for(int i=0 ; i <nums.size() ; i++){
+            if(nums[i]==0) nums[i]=pz;
+            else nums[i]=prod/nums[i];         
         }
-
-        // Product of everything to the right
-        int right = 1;
-        for(int i = n - 1; i >= 0; i--) {
-            ans[i] *= right;
-            right *= nums[i];
-        }
-
-        return ans;
+        return nums;
     }
 };
+//class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+
+//         int n = nums.size();
+//         vector<int> ans(n, 1);
+
+//         // Product of everything to the left
+//         int left = 1;
+//         for(int i = 0; i < n; i++) {
+//             ans[i] = left;
+//             left *= nums[i];
+//         }
+
+//         // Product of everything to the right
+//         int right = 1;
+//         for(int i = n - 1; i >= 0; i--) {
+//             ans[i] *= right;
+//             right *= nums[i];
+//         }
+
+//         return ans;
+//     }
+// };
